046152e6781b2b6df5dc98dc23f2ff6d4625d82d
milanissmart <milanmoturu@gmail.com>
2026-01-24 09:12:26 -0800

    Create theFirst.java
    Created first code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/theFirst.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Disabled
+@Autonomous
+public class theFirst extends OpMode {
+    @Override
+    public void init() {
+    telemetry.addData("Hello","Milan Moturu!");// prints hello:world
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
