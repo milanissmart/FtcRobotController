69a8e2a92a3a0ab35a9f862d5060552253a5c005
milanissmart <milanmoturu@gmail.com>
2026-01-26 11:26:11 -0800

    Create Methods.java
    Methods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Methods extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    double square(double input) {
+        double Output = input * input;
+        if (input < 0) {
+            Output = -Output;
+        }
+        return Output;
+    }
+
+    @Override
+    public void loop() {
+        double x = gamepad1.left_stick_x;
+        telemetry.addData("normal x", x);
+        x = square(x);
+        telemetry.addData("moded x", x);
+
+    }
+}
