0e272003f30b3f207356e57679fb3a5212d9da4a
milanissmart <milanmoturu@gmail.com>
2026-01-25 17:47:52 -0800

    Update Controllers.java
    fixed errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -17,7 +18,7 @@ public class Controllers extends OpMode {
         double diff_x = gamepad1.left_stick_x - gamepad1.right_stick_x;
         double sum_trigger = gamepad1.left_trigger + gamepad1.right_trigger;
         telemetry.addData("left x",left_stick_y);
-        telemetry.addData("left y",gamepad1.left_stick_y);
+        telemetry.addData("right y",gamepad1.right_stick_y);
         telemetry.addData("is button a pressed",gamepad1.a);
         telemetry.addData("is button b pressed",gamepad1.b);
         telemetry.addData("right x",gamepad1.right_stick_x);
